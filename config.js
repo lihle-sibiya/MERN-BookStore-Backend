960ef2a45951d3596d8802330610d9c0509f166c
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-10-22 04:14:25 +0200

    fourth commit

--- config.js
+++ config.js
@@ -1,3 +1,23 @@
 export const PORT = 5000;
 
-export const mongoDBURL = 'mongodb://127.0.0.1:27017/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://lihle:12345@cluster0.rnceree.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// import { MongoClient } from 'mongodb';
+
+// export const PORT = 5000;
+// export const mongoDBURL = 'mongodb+srv://lihle:12345@cluster0.rnceree.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// async function connectToDatabase() {
+//   try {
+//     const client = new MongoClient(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true });
+//     await client.connect();
+//     console.log('Connected to MongoDB Atlas');
+//     // You can perform your database operations here
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// }
+
+// connectToDatabase().catch(console.error);
+
+// // Your server setup and other code go here
